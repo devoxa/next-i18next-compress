71ae9baefd6022ecf2fa845156c9566cce32a23f
queicherius <queicherius@gmail.com>
2021-05-24 14:57:59 +0000

    Test for compression collisions

--- src/fsBackend.ts
+++ src/fsBackend.ts
@@ -3,9 +3,23 @@ import { compressKey } from './compressKey'
 function parseLocaleFile(string: string) {
   const json = JSON.parse(string)
 
+  const compressedKeys: Record<string, string> = {}
   const compressedJson: Record<string, string> = {}
+
   Object.keys(json).forEach((key) => {
-    compressedJson[compressKey(key)] = json[key]
+    const compressedKey = compressKey(key)
+
+    // The compressed key already exists, throw a collision error
+    if (compressedKeys[compressedKey]) {
+      const message =
+        `[next-i18next-compress] Compression collision detected: ` +
+        `"${compressedKeys[compressedKey]}" and "${key}" compress to the same hash "${compressedKey}"`
+
+      throw new Error(message)
+    }
+
+    compressedKeys[compressedKey] = key
+    compressedJson[compressedKey] = json[key]
   })
 
   return compressedJson
--- tests/fsBackend.spec.ts
+++ tests/fsBackend.spec.ts
@@ -1,16 +1,25 @@
 import fsBackend from '../src/fsBackend'
+import * as compressKeyModule from '../src/compressKey'
 
-const localeJson = {
+const compressKeySpy = jest.spyOn(compressKeyModule, 'compressKey')
+
+const localeFileString = JSON.stringify({
   'Email address': 'E-Mail Adresse',
   'Forgot password': 'Passwort vergessen',
   'Or <1>start your 30-day free trial</1>':
     'Oder <1>beginne deine 30-Tage kostenlose Probephase</1>',
   'Your sign in credentials were incorrect.': 'Deine Anmeldedaten waren inkorrekt.',
-}
+})
 
 describe('fsBackend', () => {
+  const parseLocaleFile = fsBackend.backend.parse
+
   it('correctly compresses the keys of the locale file', () => {
-    const parseLocaleFile = fsBackend.backend.parse
-    expect(parseLocaleFile(JSON.stringify(localeJson, null, 2))).toMatchSnapshot()
+    expect(parseLocaleFile(localeFileString)).toMatchSnapshot()
+  })
+
+  it('throws an error if there are any compression collisions', async () => {
+    compressKeySpy.mockImplementation(() => 'foobar')
+    expect(() => parseLocaleFile(localeFileString)).toThrowErrorMatchingSnapshot()
   })
 })
