83b748b7b9294bad8573261866b31b95ac261b5a
queicherius <queicherius@gmail.com>
2021-05-28 19:50:52 +0000

    Handle JSX in astToKey helper

--- src/astToKey.ts
+++ src/astToKey.ts
@@ -7,6 +7,12 @@ export type AbstractSyntaxTree = Array<
   | BabelTypes.ArgumentPlaceholder
   | BabelTypes.TemplateElement
   | BabelTypes.TSType
+  | BabelTypes.JSXText
+  | BabelTypes.JSXExpressionContainer
+  | BabelTypes.JSXSpreadChild
+  | BabelTypes.JSXElement
+  | BabelTypes.JSXFragment
+  | BabelTypes.JSXEmptyExpression
 >
 
 export interface AstToKeyOptions {
@@ -17,6 +23,13 @@ export interface AstToKeyOptions {
 export function astToKey(ast: AbstractSyntaxTree, pOptions: AstToKeyOptions): string {
   const options = { level: 0, ...pOptions }
   let key = ''
+
+  // Ignore empty JSXText nodes, because they get stripped away by React
+  // and interfere with getting the correct indexes for the JSXElement nodes.
+  ast = ast.filter((astNode) => {
+    if (astNode.type === 'JSXText' && astNode.value.trim() === '') return false
+    return true
+  })
 
   for (let i = 0; i !== ast.length; i++) {
     const astNode = ast[i]
@@ -67,8 +80,43 @@ export function astToKey(ast: AbstractSyntaxTree, pOptions: AstToKeyOptions): st
       continue
     }
 
+    // This is text inside of a JSX component. It may have whitespace, which is ignored
+    // by React, so we have to clean it up to match.
+    if (astNode.type === 'JSXText') {
+      let value = astNode.value
+
+      value = value.replace(/\n *$/g, '') // Ignore trailing newlines
+      value = value.replace(/\n */g, ' ') // Turn remaining newlines into spaces
+      if (i === 0) value = value.trimStart() // Trim the start if this is the first node
+      if (i === ast.length - 1) value = value.trimEnd() // Trim the end if this is the last node
+
+      key += value
+      continue
+    }
+
+    // This is a JSX component, like `<Trans><Foo>...</Foo></Trans>`
+    if (astNode.type === 'JSXElement') {
+      const childrenKey = astToKey(astNode.children, { ...options, level: options.level + 1 })
+
+      key += `<${i}>${childrenKey}</${i}>`
+      continue
+    }
+
+    // This is an interpolated expression, like `<Trans>Foo {...}</Trans>`
+    if (astNode.type === 'JSXExpressionContainer') {
+      key += astToKey([astNode.expression], { ...options, level: options.level + 1 })
+      continue
+    }
+
+    // This is an interpolated comment, like `<Trans>Foo {/* bar */}</Trans>`. We ignore these.
+    if (astNode.type === 'JSXEmptyExpression') {
+      continue
+    }
+
     // Deliberately do not handle the following types:
-    // - CallExpression (not supported `i18next` as far as I know)
+    // - JSXSpreadChild is not supported by React/NextJS
+    // - CallExpression is not supported `i18next`
+    // - JSXFragment is not supported by `i18next`
 
     throw new UnsupportedAstTypeError(astNode.type)
   }
