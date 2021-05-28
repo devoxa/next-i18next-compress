8b3cf1c6238b36226d0e0436c5493c00cf526459
queicherius <queicherius@gmail.com>
2021-05-28 22:41:18 +0000

    Add the basic next-i18next setup

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,3 +1,17 @@
+import { GetStaticProps } from 'next'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+  props: await serverSideTranslations(locale || 'en'),
+})
+
 export default function Home() {
-  return <div>Hello, World.</div>
+  const { t } = useTranslation()
+
+  return (
+    <ul>
+      <li>{t('Hello world')}</li>
+    </ul>
+  )
 }
